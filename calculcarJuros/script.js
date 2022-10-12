f60c4fd251ca56aebf02a2a979d726908ecaf67f
TadeuViegas <tadeucosta@acad.ifma.edu.br>
2022-10-12 20:52:19 -0300

    first commit

---
+++ calculcarJuros/script.js
@@ -0,0 +1,10 @@
+function calcularJuros(){
+  TODO
+  // get os caminhos de todos os inputs
+
+  // in get resultado >>>
+  // valor de entrada = valor * 0.3 && valor * 0.9
+  // quantidade de parcelas = quant.value
+  // valor de cada parcela = (valor de entrada - valor total)/quantiade de parcelas
+
+}
