077b1fce4d5ed9d4ff0e0949c0f82d73f2360237
TadeuViegas <tadeucosta@acad.ifma.edu.br>
2022-10-13 18:26:56 -0300

    add: validations

--- calculcarJuros/script.js
+++ calculcarJuros/script.js
@@ -1,10 +1,33 @@
 function calcularJuros(){
-  TODO
-  // get os caminhos de todos os inputs
+  let n = document.querySelector('input#carro');
+  let nome = n.value;
+  let v = document.getElementById('valor');
+  let valor = v.value;
+  let p = document.getElementById('percentual');
+  let percentual = p.value;
+  let qntP = document.getElementById('parcelas');
+  let qntParcelas = qntP.value;
 
-  // in get resultado >>>
-  // valor de entrada = valor * 0.3 && valor * 0.9
-  // quantidade de parcelas = quant.value
-  // valor de cada parcela = (valor de entrada - valor total)/quantiade de parcelas
+  //validaçoes e regras de negocios
+    if(valor.length == 0 || percentual.length == 0 || qntParcelas == 0){
+      //todos os campos devem ser preenchidos
+      alert(`Nenhum campo pode ficar vazio ou igual a zero`);
+    } else if (Number(valor) <= 20.000 ){
+      //supondo que o carro mais barato é maior que 20.000 reais
+      alert('O valor do carro precisa ser maior do que 20mil');
+    } else if (Number(qntParcelas) < 3 || Number(qntParcelas) > 24){
+      //validacao da quantidade de parcelas aceitavel no sistema
+      alert('A quantidade deve ser entre 3 e 24');
+    } else if (Number(percentual) < 30 || Number(percentual) > 90){
+      alert('O percentual de entrada deve ser entre 30 e 90%');
+    } else{
+    }
+    resultado.innerHTML = `<p>_______ Os dados do cliente _______</p>`;
+    resultado.innerHTML += `<p>Veículo: ${nome}</p>`;
+    let valorEntrada = valor * percentual/100;
+    let valorParcela = (valor - valorEntrada)/qntParcelas;
+    resultado.innerHTML += `<p>Valor a ser pago: R$${valor - valorEntrada}</p>`;
+    resultado.innerHTML += `<p>Quantidade de parcelas: ${qntParcelas}</p>`;
+    resultado.innerHTML += `<p>Conta: ${qntParcelas}x de ${valorParcela}</p>`;
 
 }
